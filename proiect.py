e45aa3b3d781e17636703cd67c2f2053147cb47c
andreaghibu <ghibuandreea46@gmail.com>
2024-12-11 21:04:30 +0200

    Add files via upload

---
+++ proiect.py
@@ -0,0 +1,8 @@
+import json
+
+class Song:
+    def __init__(self, title, artist, duration, genre):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+        self.genre = genre
